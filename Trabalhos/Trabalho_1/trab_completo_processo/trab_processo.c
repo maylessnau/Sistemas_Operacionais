da1ca652e668f6463e2ccf610543215ce28d2108
barbara <brareisds@gmail.com>
2025-05-08 00:48:01 -0300

    ipc memoria compartilhada

--- Trabalhos/Trabalho_1/trab_completo_processo/trab_processo.c
+++ Trabalhos/Trabalho_1/trab_completo_processo/trab_processo.c
@@ -4,14 +4,15 @@
 #include "barrier.h"
 #include "fifo.h"
 
-#define SHM_KEY 0x1234
-#define N          5           /* Nº de processos “clientes” */
+#define SHM_KEY 0x4567
+#define NUM_PROCS  5           /* Nº de processos “clientes” */
 #define MAX_USOS   3           /* ciclos de uso do recurso   */
 
 
 typedef struct {
     FifoQT fila;
     barrier_t barr;
+    int contador;
 } shared_data_t;
 
 // Função do Proc cliente
@@ -19,7 +20,7 @@ void* ciclo_cliente(shared_data_t *S, int id) {
 
     /* --- fase de preparação / primeira barreira ---------------- */
     srand(time(NULL) ^ (pthread_self() << 16));
-    int s = rand() % N;
+    int s = rand() % NUM_PROCS;
     sleep(s);
 
     printf( "--Processo: %d chegando na barreira\n", id );
@@ -60,56 +61,58 @@ void* ciclo_cliente(shared_data_t *S, int id) {
     printf( "--Processo: %d chegando novamente na barreira\n", id );
     process_barrier(&S->barr);
     printf( "++Processo: %d saindo da barreira novamente\n", id );
+
+    S->contador += 1;
+    printf("Proc %d: contador atual (compartilhado) = %d\n", id, S->contador);
+
     
     return NULL;
 }
 
 
-int main(int argc, char *argv[]) {
+int main() {
 
-    if (argc != 2) {
-        // imprime tipo "Uso: ./programa <num_processos>"
-        fprintf(stderr, "Uso: %s <num_threads>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-
-    int n = atoi(argv[1]);  // Le o numero de processos da linha de comando 
+ // Le o numero de processos da linha de comando 
  
     // TENTATIVA DE USAR SHARED MEMORY DO LINK DO PROF (N DEU CERTO K)
-    //int shmid;
-    //  shared_data_t *shared;
+    int shmid;
+    shared_data_t *shared;
    
-    // printf("Tamanho de shared_data_t: %lu\n", sizeof(shared_data_t));
-    // printf("shmget args → key=0x%x, size=%lu, flags=0%o\n",
-    //     SHM_KEY, sizeof(shared_data_t), 0644 | IPC_CREAT);
+    printf("Tamanho de shared_data_t: %lu\n", sizeof(shared_data_t));
+    printf("shmget args → key=0x%x, size=%lu, flags=0%o\n",
+    SHM_KEY, sizeof(shared_data_t), 0644 | IPC_CREAT);
  
 
-    // shmid = shmget(SHM_KEY, sizeof(shared_data_t), IPC_CREAT|0644);
-    // if (shmid == -1) {
-    //    perror("Shared memory");
-    //    return 1;
-    // }
+    shmid = shmget(SHM_KEY, sizeof(shared_data_t), IPC_CREAT|0644);
+    if (shmid == -1) {
+        perror("Shared memory");
+        return 1;
+    }
 
-    // // Attach to the segment to get a pointer to it.
-    // shared = (shared_data_t *) shmat(shmid, NULL, 0);
-    // if (shared == (void *) -1) {
-    //     perror("Shared memory attach");
-    //     return 1;
-    // }
+    // Attach to the segment to get a pointer to it.
+    shared = (shared_data_t *) shmat(shmid, NULL, 0);
+    if (shared == (void *) -1) {
+         perror("Shared memory attach");
+        return 1;
+    }
 
-    shared_data_t *shared = mmap(NULL, sizeof(shared_data_t),
-                             PROT_READ | PROT_WRITE,
-                             MAP_SHARED | MAP_ANONYMOUS,
-                             -1, 0);
+    //shared_data_t *shared = mmap(NULL, sizeof(shared_data_t),
+    //                         PROT_READ | PROT_WRITE,
+    //                        MAP_SHARED | MAP_ANONYMOUS,
+    //                         -1, 0);
    
 
 
     /* 2. Inicializa estruturas antes do fork()       */
-    init_fifoQ(&shared->fila, n);
+    init_fifoQ(&shared->fila, NUM_PROCS);
     // Apenas o processo pai inicializa a barreira (main)
-    init_barr(&shared->barr, n);
+    init_barr(&shared->barr, NUM_PROCS);
 
+    // Inicializa a semente do gerador de números aleatórios
+    srand(time(NULL));
 
+    // Gera um número aleatório entre 0 e 99
+    int recurso = rand() % 100;
     // IMPLEMENTAR
     // inicializa também a variavel inteira (recurso) com um numero
     // aleatorio indicando o numero do recurso a ser usado. 
@@ -118,11 +121,11 @@ int main(int argc, char *argv[]) {
     // assim a variável recurso NAO precisa estar em shared memory.
     // int recurso = ... numero aleatorio inteiro qualquer 
 
-
+    shared->contador = 0;  // inicia com 0
     int nProc = 0;  
     // fork para criar os processos filhos
-    pid_t pids[n];
-    for (int i = 1; i < n; i++) {
+    pid_t pids[NUM_PROCS];
+    for (int i = 1; i < NUM_PROCS; i++) {
         pid_t pid = fork();
         if (pid == 0) {
             nProc = i;
@@ -139,12 +142,12 @@ int main(int argc, char *argv[]) {
         ciclo_cliente(shared, 0);  // pai participa  -- DUVIDA: o pai tambem participa?
     
         // Espera cada filho terminar, na ordem que terminarem
-        for (int i = 1; i < n; ++i) {
+        for (int i = 1; i < NUM_PROCS; ++i) {
             int status;
             pid_t pid_terminou = wait(&status);  // espera qualquer filho
     
             // Descobre o número lógico correspondente ao PID
-            for (int j = 1; j < n; ++j) {
+            for (int j = 1; j < NUM_PROCS; ++j) {
                 if (pids[j] == pid_terminou) {
                     printf("+++ Filho de número lógico %d e pid %d terminou!\n", j, pid_terminou);
                     break;
